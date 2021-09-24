f41bb928a3b691d617dc48570630742d3b73d797
deasisrj1 <rdeasis@ualberta.ca>
2021-09-24 13:13:14 -0600

    update

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ import socketserver
 import mimetypes
 import os
 from urllib.parse import urlparse
+from urllib.error import HTTPError
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -30,141 +31,89 @@ from urllib.parse import urlparse
 
 
 class MyWebServer(socketserver.BaseRequestHandler):
+    def error_msg(self, code, msg):
+        msg = """
+            <html>
+            <body>
+            <h1>%s %s</h1>
+            </body>
+            </html>
+        """ % (code, msg)
+        return msg
+    def send_message(self, request, msg, status, status_text, type):
+        response_proto = "HTTP/1.1"
+        response_status = status
+        response_status_text = status_text
+        r = "%s %s %s\r\n" % (response_proto, response_status, response_status_text)
+        response_headers = {
+            "Content-Type": "%s; ecoding=utf-8" % type,
+            "Content-Length": len(msg),
+            "Connection": "close",
+        }
+        response_headers_raw = "".join("%s: %s\r\n" % (k, v) for k, v in response_headers.items())
+        request.send(bytearray(r,"utf-8"))
+        request.send(bytearray(response_headers_raw,"utf-8"))
+        request.send(bytearray("\r\n","utf-8"))
+        request.send(bytearray(msg,"utf-8"))
+        request.close()
     
     def handle(self):
         try:
             self.data = self.request.recv(1024).strip()
             print ("Got a request of: %s\n" % self.data)
-            # print(self.request)
-            # print(self.server)
-            # print(self.client_address)
-            # print(self.data.decode("utf-8").split()[1])
-            # print(self.data.decode("utf-8").split()[0])
             data = self.data.decode("utf-8")
 
             if len(data) == 0:
                 return
-            # get_path = "/"
+
             if len(data) > 0:
                 get_path = self.data.decode("utf-8").split()[1]
                 request = self.data.decode("utf-8").split()[0]
-                print(urlparse(get_path))
-
+                if "../" in get_path:
+                    response_status = "404"
+                    response_status_text = "Not FOUND!"
+                    msg = self.error_msg(response_status, response_status_text)
+                    self.send_message(self.request, msg, response_status, response_status_text, "text/html")
                 if get_path[-1] == "/":
                     new_path = get_path[0:-1]
                     split = get_path.split("/")
-                    print(get_path)
-                    print(split)
                     if ".html" not in split[-1]:
                         get_path = "%s/index.html" % new_path
                     else:
                         get_path =  get_path
-
-
                 if get_path == "/":
                     get_path = "/index.html"
                 
-
-
-            print(get_path)
-
-
             cur_dir = os.path.dirname(__file__)
             file_path = os.path.join(cur_dir, "www" + get_path)
             f = open(file_path, "r")
 
-            # print(f.read())
-
             mimetype, _ = mimetypes.guess_type(file_path)
             msg = f.read()
-            # f.close()
 
-            response_proto = "HTTP/1.1"
             response_status = "200"
-            response_status_text = "OK" # this can be random
-
+            response_status_text = "OK"
             if request.upper() in ["POST", "PUT", "DELETE"]:
                 response_status = "405"
                 response_status_text = "405 Not FOUND!"
-                msg = """
-                    <html>
-                    <body>
-                    <h1>405 Not FOUND!</h1>
-                    </body>
-                    </html>
-                    """
+                msg = self.error_msg(response_status, response_status_text)
+            self.send_message(self.request, msg, response_status, response_status_text, mimetype)
+            f.close()
 
-            r = "%s %s %s\r\n" % (response_proto, response_status, response_status_text)
-            response_headers = {
-                "Content-Type": "%s; ecoding=utf-8" % mimetype,
-                "Content-Length": len(msg),
-                "Connection": "close",
-            }
-
-            response_headers_raw = "".join("%s: %s\r\n" % (k, v) for k, v in response_headers.items())
-
-            self.request.send(bytearray(r,"utf-8"))
-            self.request.send(bytearray(response_headers_raw,"utf-8"))
-            self.request.send(bytearray("\r\n","utf-8"))
-            self.request.send(bytearray(msg,"utf-8"))
-            self.request.close()
         except (IsADirectoryError) as e:
-            msg = """
-            <html>
-            <body>
-            <h1>404 Not FOUND!</h1>
-            </body>
-            </html>
-            """
-            response_headers = {
-                "Content-Type": "text/html; ecoding=utf-8",
-                "Content-Length": len(msg),
-                "Connection": "close",
-            }
-
-            response_headers_raw = "".join("%s: %s\r\n" % (k, v) for k, v in response_headers.items())
-
-            response_proto = "HTTP/1.1"
             response_status = "303"
-            response_status_text = "Not FOUND!" # this can be random
-            r = "%s %s %s\r\n" % (response_proto, response_status, response_status_text)
+            response_status_text = "Not FOUND!"
+            msg = self.error_msg(response_status, response_status_text)
+            self.send_message(self.request, msg, response_status, response_status_text, "text/html")
 
-            self.request.send(bytearray(r,"utf-8"))
-            self.request.send(bytearray(response_headers_raw,"utf-8"))
-            self.request.send(bytearray("\r\n","utf-8"))
-            self.request.send(bytearray(msg,"utf-8"))
-            self.request.close()
         except( FileNotFoundError, NotADirectoryError )as e:
-            msg = """
-            <html>
-            <body>
-            <h1>404 Not FOUND!</h1>
-            </body>
-            </html>
-            """
-            response_headers = {
-                "Content-Type": "text/html; ecoding=utf-8",
-                "Content-Length": len(msg),
-                "Connection": "close",
-            }
-
-            response_headers_raw = "".join("%s: %s\r\n" % (k, v) for k, v in response_headers.items())
-
-            response_proto = "HTTP/1.1"
             response_status = "404"
-            response_status_text = "Not FOUND!" # this can be random
-
-            r = "%s %s %s\r\n" % (response_proto, response_status, response_status_text)
-
-            self.request.send(bytearray(r,"utf-8"))
-            self.request.send(bytearray(response_headers_raw,"utf-8"))
-            self.request.send(bytearray("\r\n","utf-8"))
-            self.request.send(bytearray(msg,"utf-8"))
-            self.request.close()
+            response_status_text = "Not FOUND!" 
+            msg = self.error_msg(response_status, response_status_text)
+            self.send_message(self.request, msg, response_status, response_status_text, "text/html")
 
         except Exception as e:
             print(e)
-        # FileNotFoundError, IsADirectoryError
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
